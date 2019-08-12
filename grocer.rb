b5ed9b463690315944ab1b9de536c8f63772900d
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-12 23:26:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,17 +1,5 @@
 def consolidate_cart(cart)
- final = []
- count = :count
- cart.each do |food, description|
-   if final.has_key?(fod) == false 
-    final[food] = description
-     final[food][count] = 1 
-   elsif final.has_key?(food)
-   final[food][:count] += 1 
- end
- end
- end
- final
- end
+
  
 
 def apply_coupons(cart, coupons)
